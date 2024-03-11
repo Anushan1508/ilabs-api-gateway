f4e50ee228398cad0584e6fc70863f261504b8c0
anushan1508 <anushan1508@gmail.com>
2024-03-12 02:50:33 +0530

    First

--- src/main/java/com/example/apigateway/domain/dto/login/LoginResponse.java
+++ src/main/java/com/example/apigateway/domain/dto/login/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.example.apigateway.domain.dto.login;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginResponse {
     private String responseId;
+    @JsonProperty("resultCode")
+    private String resultCode;
+    @JsonProperty("resultDesc")
+    private String  resultDesc;
     private String token;
-    private String username;
-    private String role;
-    private String email;
-    private String phone;
-    private String address;
-    private String name;
-    private String surname;
+//    private String username;
+//    private String role;
+//    private String email;
+//    private String phone;
+//    private String address;
+//    private String name;
+//    private String surname;
 }
